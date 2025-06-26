42a264f6dcd97eebeee61d02a57f6d5af995a215
ShinhyeL <llsh1123@naver.com>
2025-06-26 16:50:59 +0900

    table

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/Busbomproject_autogen/include/ui_mainwindow.h
+++ build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/Busbomproject_autogen/include/ui_mainwindow.h
@@ -35,7 +35,7 @@ public:
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 800, 26));
+        menubar->setGeometry(QRect(0, 0, 800, 22));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
