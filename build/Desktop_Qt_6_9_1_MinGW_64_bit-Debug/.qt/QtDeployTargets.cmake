a39e1c6df996c25ea36e1c46b519442bdc47d5f0
ShinhyeL <112687898+ShinhyeL@users.noreply.github.com>
2025-06-27 10:03:03 +0900

    Delete build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Busbomproject_FILE C:/Users/2-06/Documents/Busbomproject/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/Busbomproject.exe)
-set(__QT_DEPLOY_TARGET_Busbomproject_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Busbomproject_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
